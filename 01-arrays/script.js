cf3377eacb35986239f4b47de9424d80be0c21f0
VincenzoIuliano <bubucane@outlook.it>
2024-10-16 15:53:15 +0200

    EDIT: last solution

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -51,5 +51,5 @@ console.log (teachersString)
 
 // 10. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = teachers.isEmpty;
+const isTeachersEmpty = teachers.lengt === 0;
 console.log (isTeachersEmpty)
